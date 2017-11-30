b5d1735dbb4be0f8236121a6ab054cddaef7c2ba
fanitriastowo <fani.triastowo@gmail.com>
2017-11-30 13:36:19 +0700

    added closed pageQ

---
+++ protected/application/views/closed.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Lomba Akuntansi UMP 2017</title>
+    
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+</head>
+<style type="text/css">
+    body {
+        background-color: #f0f0f2;
+        margin: 0;
+        padding: 0;
+        font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+        
+    }
+    div {
+        width: 600px;
+        margin: 5em auto;
+        padding: 50px;
+        background-color: #fff;
+        border-radius: 1em;
+    }
+    a:link, a:visited {
+        color: #38488f;
+        text-decoration: none;
+    }
+    @media (max-width: 700px) {
+        body {
+            background-color: #fff;
+        }
+        div {
+            width: auto;
+            margin: 0 auto;
+            border-radius: 0;
+            padding: 1em;
+        }
+    }
+    </style>   
+<body>
+
+    <div>
+        <h1>Lomba Akuntansi</h1>
+        <p>Terima kasih telah mengikuti Lomba Akuntansi UMP 2017. Sampai jumpa tahun depan. :-)</p>
+    </div>
+
+</body>
+</html>
